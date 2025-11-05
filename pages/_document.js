5c0cea6db2b5cea48e7c045e260815ea6f6f7057
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2025-11-05 18:04:06 +0200

    sad

--- pages/_document.js
+++ pages/_document.js
@@ -164,6 +164,85 @@
 
 // export default MyDocument;
 
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+
+// class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const initialProps = await Document.getInitialProps(ctx);
+//     return { ...initialProps, locale: ctx?.locale || "en" };
+//   }
+
+//   render() {
+//     const isProduction = process.env.NODE_ENV === "production";
+//     const clarityID = process.env.NEXT_PUBLIC_CLARITY_ID;
+//     return (
+//       <Html
+//         dir={this.props.locale === "ar" ? "rtl" : "ltr"}
+//         lang={this.props.locale}
+//       >
+//         <Head>
+//           <link rel="shortcut icon" href="/favicon.ico" />
+//           <link rel="preconnect" href="https://fonts.googleapis.com" />
+//           <link rel="preconnect" href="https://fonts.gstatic.com" />
+//           <link
+//             href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;900&family=Inter:wght@300;400;500;600;700&display=swap"
+//             rel="stylesheet"
+//           />
+//           {/* Font Awesome is now imported locally from npm in _app.js to reduce CSP scope */}
+//           {/* Load Clarity script only in production */}
+//           <script
+//             dangerouslySetInnerHTML={{
+//               __html:
+//                 "window.$zoho=window.$zoho||{};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}",
+//             }}
+//           />
+//           <script
+//             id="zsiqscript"
+//             src="https://salesiq.zoho.com/widget?wc=siqafbecdd73009757c0f4d341614b6d2bc64b57ca334e6134664a76932173e11caa9ec28430f7ea12a0c2a3fb1d0a131d0"
+//             defer
+//           />
+//           {isProduction && clarityID && (
+//             <>
+//               <script
+//                 type="text/javascript"
+//                 dangerouslySetInnerHTML={{
+//                   __html: `
+//                 (function(c,l,a,r,i,t,y){
+//                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+//                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+//                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+//                 })(window, document, "clarity", "script", "${clarityID}");
+//                 `,
+//                 }}
+//               />
+//               <script
+//                 async
+//                 src={`https://www.googletagmanager.com/gtag/js?id=G-V3S8895Y82`}
+//               />
+//               <script
+//                 dangerouslySetInnerHTML={{
+//                   __html: `
+//                 window.dataLayer = window.dataLayer || [];
+//                 function gtag(){dataLayer.push(arguments);}
+//                 gtag('js', new Date());
+//                 gtag('config', 'G-V3S8895Y82');
+//               `,
+//                 }}
+//               />
+//             </>
+//           )}
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// export default MyDocument;
+
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -173,8 +252,6 @@ class MyDocument extends Document {
   }
 
   render() {
-    const isProduction = process.env.NODE_ENV === "production";
-    const clarityID = process.env.NEXT_PUBLIC_CLARITY_ID;
     return (
       <Html
         dir={this.props.locale === "ar" ? "rtl" : "ltr"}
@@ -182,55 +259,7 @@ class MyDocument extends Document {
       >
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;900&family=Inter:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          {/* Font Awesome is now imported locally from npm in _app.js to reduce CSP scope */}
-          {/* Load Clarity script only in production */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-                "window.$zoho=window.$zoho||{};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}",
-            }}
-          />
-          <script
-            id="zsiqscript"
-            src="https://salesiq.zoho.com/widget?wc=siqafbecdd73009757c0f4d341614b6d2bc64b57ca334e6134664a76932173e11caa9ec28430f7ea12a0c2a3fb1d0a131d0"
-            defer
-          />
-          {isProduction && clarityID && (
-            <>
-              <script
-                type="text/javascript"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "${clarityID}");
-                `,
-                }}
-              />
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=G-V3S8895Y82`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);} 
-                gtag('js', new Date());
-                gtag('config', 'G-V3S8895Y82');
-              `,
-                }}
-              />
-            </>
-          )}
+          {/* Self-hosted Font Awesome and Fonts */}
         </Head>
         <body>
           <Main />
