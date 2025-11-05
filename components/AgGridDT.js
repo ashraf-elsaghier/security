b71e0ec21b8e429bf725ffc0f3b9ee94994f6339
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2025-11-05 14:37:45 +0200

    fix

--- components/AgGridDT.js
+++ components/AgGridDT.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 //Ag grid
 import { AgGridReact } from "ag-grid-react";
 
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 // import "ag-grid-community/styles/ag-theme-alpine-dark.css";
 
 import { useSelector } from "react-redux";
